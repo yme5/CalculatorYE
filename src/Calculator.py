290c65898c67fa792e83132376b6de2e23355b16
yme5 <yme5@njit.edu>
2021-03-23 22:19:50 -0400

    Feature: Setup calculator with result property

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,4 +1,6 @@
 class Calculator:
     result = 0
     def __init__(self):
+        x = 2 + 2
+        self.result = x;
         pass
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -9,6 +9,11 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_results_property_calculator(self):
+        calculator = Calculator()
+        self.assertEquals(calculator.result, 4)
+
+
 
 if __name__ == '__main__':
     unittest.main()
